f654f967588b0e903300f4c91e28e6a891f23c74
MiglyA <zq6295migly@gmail.com>
2019-06-12 22:52:26 +0900

    [update]kinectで骨格推定できた

--- src/kinect.cpp
+++ src/kinect.cpp
@@ -5,43 +5,79 @@
 #include <pcl/PCLPointCloud2.h>
 #include <pcl/conversions.h>
 #include <pcl_ros/transforms.h>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/highgui.hpp>
 
 
 class TFPose_Kinect {
 public:
 	TFPose_Kinect();
 	~TFPose_Kinect();
+	cv::Mat color;
+	cv::Mat depth;
+	cv::Mat view_depth;
+
 private:
 	ros::NodeHandle n;
-	ros::Publisher cloud_image;
+	ros::Publisher kinect_image;
 	ros::Subscriber point_cloud_data;
+
+	double hypot_3d(double x, double y, double z) {
+		return sqrt(x * x + y * y + z * z);
+	}
 
 	void point_cloud_data_callback(const boost::shared_ptr<const sensor_msgs::PointCloud2>& input) {
 
+		int height = (int)input->height;
+		int width = (int)input->width;
+		double x, y, z;
+
+		color = cv::Mat::zeros(cv::Size(width, height), CV_8UC3);
+		depth = cv::Mat::zeros(cv::Size(width, height), CV_64F);
+
+		//pcl変換
 		pcl::PCLPointCloud2 pcl_pc2;
 		pcl_conversions::toPCL(*input, pcl_pc2);
 		pcl::PointCloud<pcl::PointXYZRGB>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
 		pcl::fromPCLPointCloud2(pcl_pc2, *temp_cloud);
-		//do stuff with temp_cloud here
-		printf("%d\n", temp_cloud->points[10000].r );
+
+		//展開
+		for (int w = 0; w < width; ++w) {
+			for (int h = 0; h < height; ++h) {
+				color.at<cv::Vec3b>(h, w)[0] = (int)temp_cloud->points[width * h + w].b;
+				color.at<cv::Vec3b>(h, w)[1] = (int)temp_cloud->points[width * h + w].g;
+				color.at<cv::Vec3b>(h, w)[2] = (int)temp_cloud->points[width * h + w].r;
+				x = temp_cloud->points[width * h + w].x;
+				y = temp_cloud->points[width * h + w].y;
+				z = temp_cloud->points[width * h + w].z;
+				depth.at<double>(h, w) = hypot_3d(x, y, z);
+			}
+		}
+
+		depth.convertTo(view_depth, CV_8UC1, 255.0 / 4.0);
+
+		cv::namedWindow("depth", CV_WINDOW_NORMAL);
+		cv::imshow("depth", view_depth);
+
+		cv::namedWindow("color", CV_WINDOW_NORMAL);
+		cv::imshow("color", color);
+
+		cv::waitKey(1);
+
+		kinect_image.publish(cv_bridge::CvImage(std_msgs::Header(), "bgr8", color));
 
 	}
 
 };
 
 TFPose_Kinect::TFPose_Kinect() {
-	//ros::Publisher pub_points = n.advertise<std_msgs::Float64MultiArray>("/ros_kinect/points", 1000);
-
-	point_cloud_data = n.subscribe("/camera/depth_registered/points", 1000, &TFPose_Kinect::point_cloud_data_callback, this);
-
+	kinect_image = n.advertise<sensor_msgs::Image>("/tf_pose/kinect_image", 1);
+	point_cloud_data = n.subscribe("/camera/depth_registered/points", 1, &TFPose_Kinect::point_cloud_data_callback, this);
 }
 
 TFPose_Kinect::~TFPose_Kinect() {
 
 }
-
-
-
 
 int main(int argc, char** argv) {
 
@@ -49,8 +85,6 @@ int main(int argc, char** argv) {
 
 	TFPose_Kinect kinect;
 
-	while (ros::ok()) {
-		ros::spinOnce();
-	}
+	ros::spin();
 
 }
