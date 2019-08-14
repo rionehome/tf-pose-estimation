afc3760549dbce08552465e69b1759256ebd6787
MiglyA <zq6295migly@gmail.com>
2019-08-14 18:35:05 +0900

    [update]メッセージ変更

--- scripts/broadcaster_ros.py
+++ scripts/broadcaster_ros.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
-import time
+# -*- coding: utf-8 -*-
 import os
 import sys
-import ast
 
 from threading import Lock
 import rospy
 import rospkg
 from cv_bridge import CvBridge, CvBridgeError
-from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from tfpose_ros.msg import Persons, Person, BodyPartElm
 
@@ -16,83 +14,90 @@ from tf_pose.estimator import TfPoseEstimator
 from tf_pose.networks import model_wh, get_graph_path
 
 
-def humans_to_msg(humans):
-	persons = Persons()
-
-	for human in humans:
-		person = Person()
-
-		for k in human.body_parts:
-			body_part = human.body_parts[k]
-
-			body_part_msg = BodyPartElm()
-			body_part_msg.part_id = body_part.part_idx
-			body_part_msg.x = body_part.x
-			body_part_msg.y = body_part.y
-			body_part_msg.confidence = body_part.score
-			person.body_part.append(body_part_msg)
-		persons.persons.append(person)
-
-	return persons
-
-
-def callback_image(data):
-	# et = time.time()
-	try:
-		cv_image = cv_bridge.imgmsg_to_cv2(data, "bgr8")
-	except CvBridgeError as e:
-		rospy.logerr('[tf-pose-estimation] Converting Image Error. ' + str(e))
-		return
-
-	acquired = tf_lock.acquire(False)
-	if not acquired:
-		return
-
-	try:
-		humans = pose_estimator.inference(cv_image, resize_to_default=True, upsample_size=resize_out_ratio)
-	finally:
-		tf_lock.release()
-
-	msg = humans_to_msg(humans)
-	msg.image_w = data.width
-	msg.image_h = data.height
-	msg.header = data.header
-
-	pub_pose.publish(msg)
+class BroadCaster:
+    def __init__(self):
+        
+        rospy.loginfo('initialization+')
+        rospy.init_node('TfPoseEstimatorROS', anonymous=True, log_level=rospy.INFO)
+        # parameters
+        self.model = rospy.get_param('~model', 'cmu')
+        self.resolution = rospy.get_param('~resolution', '432x368')
+        self.resize_out_ratio = float(rospy.get_param('~resize_out_ratio', '4.0'))
+        __image_topic__ = rospy.get_param('~camera', '')
+        
+        if not __image_topic__:
+            rospy.logerr('Parameter \'camera\' is not provided.')
+            sys.exit(-1)
+        
+        self.tf_lock = Lock()
+        self.cv_bridge = CvBridge()
+        self.pose_estimator = self.__read_model__()
+        self.cv_image = None
+        
+        rospy.Subscriber(__image_topic__, Image, self.callback_image, queue_size=1, buff_size=2 ** 24)
+        self.pose_pub = rospy.Publisher('/estimation/output', Persons, queue_size=1)
+    
+    def __read_model__(self):
+        # model読み込み
+        print "model読み込み"
+        try:
+            w, h = model_wh(self.resolution)
+            graph_path = get_graph_path(self.model)
+            ros_pack = rospkg.RosPack()
+            graph_path = os.path.join(ros_pack.get_path('tfpose_ros'), graph_path)
+            pose_estimator = TfPoseEstimator(graph_path, target_size=(w, h))
+            return pose_estimator
+        except Exception as e:
+            rospy.logerr('invalid model: %s, e=%s' % (self.model, e))
+            sys.exit(-1)
+    
+    @staticmethod
+    def humans_to_msg(humans, msg):
+        # type:(list,Image)->Persons
+        persons = Persons()
+        persons.header = msg.header
+        persons.image_w = msg.width
+        persons.image_h = msg.height
+        
+        for human in humans:
+            person = Person()
+            
+            for k in human.body_parts:
+                body_part = human.body_parts[k]
+                
+                body_part_msg = BodyPartElm()
+                body_part_msg.part_id = body_part.part_idx
+                body_part_msg.x = body_part.x * msg.width
+                body_part_msg.y = body_part.y * msg.height
+                body_part_msg.confidence = body_part.score
+                person.body_part.append(body_part_msg)
+            persons.persons.append(person)
+        return persons
+    
+    def callback_image(self, msg):
+        
+        try:
+            self.cv_image = self.cv_bridge.imgmsg_to_cv2(msg, "bgr8")
+        except CvBridgeError as e:
+            rospy.logerr('[tf-pose-estimation] Converting Image Error. ' + str(e))
+        
+        acquired = self.tf_lock.acquire(False)
+        if not acquired:
+            return
+        
+        try:
+            humans = self.pose_estimator.inference(self.cv_image, resize_to_default=True,
+                                                   upsample_size=self.resize_out_ratio)
+        finally:
+            self.tf_lock.release()
+        
+        result = self.humans_to_msg(humans, msg)
+        
+        self.pose_pub.publish(result)
 
 
 if __name__ == '__main__':
-	rospy.loginfo('initialization+')
-	rospy.init_node('TfPoseEstimatorROS', anonymous=True, log_level=rospy.INFO)
-
-	# parameters
-	image_topic = rospy.get_param('~camera', '')
-	model = rospy.get_param('~model', 'cmu')
-
-	resolution = rospy.get_param('~resolution', '432x368')
-	resize_out_ratio = float(rospy.get_param('~resize_out_ratio', '4.0'))
-	tf_lock = Lock()
-
-	if not image_topic:
-		rospy.logerr('Parameter \'camera\' is not provided.')
-		sys.exit(-1)
-
-	try:
-		w, h = model_wh(resolution)
-		graph_path = get_graph_path(model)
-
-		rospack = rospkg.RosPack()
-		graph_path = os.path.join(rospack.get_path('tfpose_ros'), graph_path)
-	except Exception as e:
-		rospy.logerr('invalid model: %s, e=%s' % (model, e))
-		sys.exit(-1)
-
-	pose_estimator = TfPoseEstimator(graph_path, target_size=(w, h))
-	cv_bridge = CvBridge()
-
-	rospy.Subscriber(image_topic, Image, callback_image, queue_size=1, buff_size=2 ** 24)
-	pub_pose = rospy.Publisher('~pose', Persons, queue_size=1)
-
-	rospy.loginfo('start+')
-	rospy.spin()
-	rospy.loginfo('finished')
+    BroadCaster()
+    rospy.loginfo('start+')
+    rospy.spin()
+    rospy.loginfo('finished')
--- scripts/viewer.py
+++ scripts/viewer.py
@@ -5,22 +5,22 @@ import copy
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
 import rospy
-from ros_posenet.msg import Poses
 from sensor_msgs.msg import Image
+from tfpose_ros.msg import *
 
 
 class Visualize:
     def __init__(self):
         rospy.init_node("visualize")
-        rospy.Subscriber("/ros_posenet/result", Poses, self.poses_callback, queue_size=1)
-        rospy.Subscriber("/posenet/input", Image, self.image_callback, queue_size=1)
+        rospy.Subscriber("/estimation/output", Poses, self.poses_callback, queue_size=1)
+        rospy.Subscriber("/usb_cam/image_raw", Image, self.image_callback, queue_size=1)
         self.bridge = CvBridge()
         self.color_image = None
-        self.poses = None
+        self.persons = None
     
     def poses_callback(self, msg):
-        # type:(Poses)->None
-        self.poses = msg.poses
+        # type:(Persons)->None
+        self.persons = msg.persons
     
     def image_callback(self, msg):
         # type:(Image)->None
@@ -30,27 +30,19 @@ class Visualize:
         except CvBridgeError as e:
             print 'Cv_Bridge_Error:', e
         
-        if self.poses is None:
+        if self.persons is None:
             cv2.imshow("window", self.color_image)
             cv2.waitKey(1)
             return
         
         person_id = 0
-        for pose in self.poses:
-            for key in pose.keypoints:
+        for person in self.persons:
+            for body in person.body_part:
                 color_table = {0: (255, 0, 0), 1: (0, 255, 0), 2: (0, 0, 255), 3: (255, 0, 255), 4: (255, 255, 0)}
-                cv2.circle(self.color_image, (int(key.image_position.x), int(key.image_position.y)), 10,
-                           (color_table[person_id]),
+                cv2.circle(self.color_image, (int(body.x), int(body.y)), 10, (color_table[person_id]),
                            thickness=-1)
-                cv2.putText(self.color_image,
-                            str((float("{0:.2f}".format(key.position.x)), float("{0:.2f}".format(key.position.y)),
-                                 float("{0:.2f}".format(key.position.z)))),
-                            (int(key.image_position.x), int(key.image_position.y)),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                            (255, 255, 255), 1, cv2.LINE_AA)
             person_id += 1
         
-        del self.poses[:]
         cv2.imshow("window", self.color_image)
         cv2.waitKey(1)
 
