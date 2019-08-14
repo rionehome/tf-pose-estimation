8166d81c4505e0bac0d9c54a3c286308cb03dd0e
MiglyA <zq6295migly@gmail.com>
2019-08-14 18:55:59 +0900

    [update]メッセージ名の見直し

--- scripts/broadcaster_ros.py
+++ scripts/broadcaster_ros.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
-import sys
 
 from threading import Lock
 import rospy
 import rospkg
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
-from tfpose_ros.msg import Persons, Person, BodyPartElm
+from tfpose_ros.msg import *
 
 from tf_pose.estimator import TfPoseEstimator
 from tf_pose.networks import model_wh, get_graph_path
@@ -23,19 +22,14 @@ class BroadCaster:
         self.model = rospy.get_param('~model', 'cmu')
         self.resolution = rospy.get_param('~resolution', '432x368')
         self.resize_out_ratio = float(rospy.get_param('~resize_out_ratio', '4.0'))
-        __image_topic__ = rospy.get_param('~camera', '')
-        
-        if not __image_topic__:
-            rospy.logerr('Parameter \'camera\' is not provided.')
-            sys.exit(-1)
         
         self.tf_lock = Lock()
         self.cv_bridge = CvBridge()
         self.pose_estimator = self.__read_model__()
         self.cv_image = None
         
-        rospy.Subscriber(__image_topic__, Image, self.callback_image, queue_size=1, buff_size=2 ** 24)
-        self.pose_pub = rospy.Publisher('/estimation/output', Persons, queue_size=1)
+        rospy.Subscriber('/usb_cam/image_raw', Image, self.callback_image, queue_size=1, buff_size=2 ** 24)
+        self.pose_pub = rospy.Publisher('/tfpose_ros/output', Poses, queue_size=1)
     
     def __read_model__(self):
         # model読み込み
@@ -53,29 +47,23 @@ class BroadCaster:
     
     @staticmethod
     def humans_to_msg(humans, msg):
-        # type:(list,Image)->Persons
-        persons = Persons()
-        persons.header = msg.header
-        persons.image_w = msg.width
-        persons.image_h = msg.height
-        
+        # type:(list,Image)->Poses
+        poses = Poses()
         for human in humans:
-            person = Person()
-            
-            for k in human.body_parts:
-                body_part = human.body_parts[k]
-                
-                body_part_msg = BodyPartElm()
-                body_part_msg.part_id = body_part.part_idx
-                body_part_msg.x = body_part.x * msg.width
-                body_part_msg.y = body_part.y * msg.height
-                body_part_msg.confidence = body_part.score
-                person.body_part.append(body_part_msg)
-            persons.persons.append(person)
-        return persons
+            pose = Pose()
+            for body in human.body_parts:
+                body_part = human.body_parts[body]
+                key_msg = Keypoint()
+                key_msg.part = str(body_part.part_idx)
+                key_msg.image_position.x = body_part.x * msg.width
+                key_msg.image_position.y = body_part.y * msg.height
+                key_msg.score = body_part.score
+                pose.keypoints.append(key_msg)
+            poses.poses.append(pose)
+        return poses
     
     def callback_image(self, msg):
-        
+        # type:(Image)->None
         try:
             self.cv_image = self.cv_bridge.imgmsg_to_cv2(msg, "bgr8")
         except CvBridgeError as e:
